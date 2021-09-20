2f236af239ec944f5b9d1fa1f4f40669f926e189
mjshah13 <m.jshah13@gmail.com>
2021-09-21 00:36:43 +0500

    Deployment Build Added

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,5 @@
 const router = require('express').Router();
 
-router.get("/", (req, res) => res.send("Welcome"));
 router.use('/playCards', require('./play-cards'));
 router.use('/playCardsAdmin', require('./play-cards-admin'));
 
--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const path =require('path');
 require('./config/init-firebase');
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 40;
 app.use(cors());
 app.use(express.json());
 app.use("/", require('./routes'));
-
+// Modifications
+app.use(express.static(path.join(__dirname, '/build')));
+// Handles any requests that don't match the ones above
+app.get('*', (req,res) =>{
+    res.sendFile(path.join(__dirname+'/build/index.html'));
+});
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
 });
